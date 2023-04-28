58ebb67c2488702b59bfc4f19644ddf0161081cc
ItIsLukenTime <lmuhnick@stevens.edu>
2023-04-27 21:09:05 -0400

    Got users working; made revisions to seed file

--- data/users.js
+++ data/users.js
@@ -1,37 +1,36 @@
 const mongoCollections = require("../config/mongoCollections");
 const users = mongoCollections.users;
 
-
-let exportedMethod = {
-
-    /*  USERNAME VALIDATION
+/*  USERNAME VALIDATION
     Has at least 6 characters
     Contains at least one letter and one number
     Returns true/false on validity! */
 
-    async validName(name) {
-        if (name.length < 6) { return false; }
-    
-        /* Username must contain at least one letter and one number */
-        if (/[a-zA-Z]/g.test(name) === false || /^\d+$/.test(name) === false) { return false; }
-    
-        return true;
-    },
+function validName(name) {
+    if (name.length < 6) { return false; }
+
+    /* Username must contain at least one letter and one number */
+    if (/[a-zA-Z]/g.test(name) === false || /\d/.test(name) === false) { return false; }
+
+    return true;
+};
 
 
-    /*  PASSWORD VALIDATION
+/*  PASSWORD VALIDATION
     Has at least 8 characters
     Contains at least one letter and one number
     Returns true/false on validity */
 
-    async validPassword(password) {
-        if (password.length < 8) { return false; }
+function validPassword(password) {
+    if (password.length < 8) { return false; }
 
-        /* Password must contain at least one letter and one number */
-        if (/[a-zA-Z]/g.test(name) === false || /^\d+$/.test(name) === false) { return false; }
+    /* Password must contain at least one letter and one number */
+    if (/[a-zA-Z]/g.test(password) === false || /\d/.test(password) === false) { return false; }
 
-        return true;
-    },
+    return true;
+}
+
+let exportedMethod = {
 
     /*  CREATE USER
     The createUser function takes in a username and password
@@ -57,8 +56,10 @@ let exportedMethod = {
         /* Step 2: Insert user */
     
         const newUser = {username: username, password: password};
+
+        const user = await users();
     
-        const insertInfo = await newUser.insertOne(newUser);
+        const insertInfo = await user.insertOne(newUser);
         if (!insertInfo.acknowledged || !insertInfo.insertedId) { throw 'Error: Could not add user'; }
     
     
@@ -68,68 +69,3 @@ let exportedMethod = {
 }
 
 module.exports = exportedMethod;
-
-
-// export const validName = (name) => {
-
-//     if (name.length < 6) { return false; }
-    
-//     /* Username must contain at least one letter and one number */
-//     if (/[a-zA-Z]/g.test(name) === false || /^\d+$/.test(name) === false) { return false; }
-   
-//     return true;
-
-// };
-
-
-/*  PASSWORD VALIDATION
-    Has at least 8 characters
-    Contains at least one letter and one number
-    Returns true/false on validity */
-
-// export const validPassword = (password) => {
-
-//     if (password.length < 8) { return false; }
-
-//     /* Password must contain at least one letter and one number */
-//     if (/[a-zA-Z]/g.test(name) === false || /^\d+$/.test(name) === false) { return false; }
-
-//     return true;
-// };
-
-
-/*  CREATE USER
-    The createUser function takes in a username and password
-    Utilizes the two helper functions above to ensure username/password validity
-    Then, it uploads it to the MongoDB database */
-
-// export const createUser = async (username, password) => {
-
-//     /* Step 1: Error check */
-
-//     if (!username || !password) {
-//         throw "Error: Username and/or password missing";
-//     }
-
-//     if (typeof username !== 'string' || typeof password !== 'string') {
-//         throw "Error: Username and/or password must be a string";
-//     }
-
-//     username = username.trim();
-//     if (validName(username) == false) { throw "Error: Username is invalid!"; }
-    
-//     password = password.trim();
-//     if (validPassword(password) == false) { throw "Error: Password is invalid!"; }
-
-
-//     /* Step 2: Insert user */
-
-//     const newUser = {username: username, password: password};
-
-//     const insertInfo = await newUser.insertOne(newBand);
-//     if (!insertInfo.acknowledged || !insertInfo.insertedId) { throw 'Error: Could not add user'; }
-
-
-//     return {created: true};
-    
-// };
--- seed.js
+++ seed.js
@@ -7,6 +7,29 @@ const main=async()=>{
         await words.addWord();
     }
 
+    try {
+        await users.createUser("hi", "hello")
+        console.log("This should not print");
+    } catch (e) {
+        console.log(e);
+        console.log("The above test case fails");
+    }
+
+    try {
+        await users.createUser("TestUser123", "545IsCool")
+        console.log("This test case passes");
+    } catch (e) {
+        console.log(e);
+    }
+
+    try {
+        await users.createUser("Luke602", "02Luke02");
+        console.log("The second user is now in the database");
+    } catch (e) {
+        console.log(e);
+    }
+
+
     const db = await connection.connectToDb();
     await connection.closeConnection();
 }
