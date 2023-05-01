d50760b3bc8b121644f3ff4ac10491c54c1ec112
cindytran211 <Cindy795Tran@gmail.com>
2023-05-01 18:52:51 -0400

    changes

--- seed.js
+++ seed.js
@@ -3,12 +3,12 @@ const users = require('./data/users.js')
 const connection = require('./config/mongoConnection');
 
 const main=async()=>{
-    for(let i=0;i<10;i++){
+    for(let i=0;i<100;i++){
         await words.addWord();
     }
 
     try {
-        await users.createUser("hi", "hello")
+        await users.createUser("name","hi", "hi@gmail","hello")
         console.log("This should not print");
     } catch (e) {
         console.log(e);
@@ -16,14 +16,14 @@ const main=async()=>{
     }
 
     try {
-        await users.createUser("TestUser123","testuser@gmail.com" ,"545IsCool")
+        await users.createUser("User","TestUser123","testuser@gmail.com" ,"545IsCool")
         console.log("This test case passes");
     } catch (e) {
         console.log(e);
     }
 
     try {
-        await users.createUser("Luke602", "luke@gmail.com","02Luke02");
+        await users.createUser("luke","Luke602", "luke@gmail.com","02Luke02");
         console.log("The second user is now in the database");
     } catch (e) {
         console.log(e);
